8f4fe18c5a8e9235d16405e07a5dad635db3751a
Markus Meyer <dev@markus.koeln>
2019-12-27 18:59:37 +0100

    BugFixes

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -19,6 +19,8 @@ sap.ui.define([
 
 		resetModel: function () {
 			let oModel = new JSONModel({
+				Busy: false,
+				Path: "",
 				ThrowFactors: [{
 						Id: "1",
 						Name: "1x"
@@ -77,8 +79,14 @@ sap.ui.define([
 				return;
 			}
 
+			let oModel = this.getView().getModel();
+			let sPath = this.classifyImage(oModel.getProperty("/Input/0/Factor"), 
+			oModel.getProperty("/Input/1/Factor"), oModel.getProperty("/Input/2/Factor"),
+			oModel.getProperty("/Input/0/Amount"), oModel.getProperty("/Input/1/Amount"), oModel.getProperty("/Input/2/Amount") );
+
+			oModel.setProperty("/Path", sPath);
 			//Daten hochladen
-			oUploader.setBusy(true);
+			this.getView().getModel().setProperty("/Busy", true);
 			oUploader.upload();
 		},
 
@@ -92,19 +100,19 @@ sap.ui.define([
 		},
 
 		handleUploadComplete: function (oEvent) {
-			let oFileUploader = this.byId("fileUploader");
-			oFileUploader.setBusy(false);
+			this.getView().getModel().setProperty("/Busy", false);
 
 			let oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
 
+			let sResponse = oEvent.getParameter("responseRaw");
 			let sStatus = oEvent.getParameter("status");
-			if (sStatus.startsWith("2")) {
+			if (sStatus == 200) {
 
 				sap.m.MessageToast.show(oResourceBundle.getText("message.upload.success"));
 
 				this._resetView();
 			} else {
-				this._showError(oSource.getParameter("response"));
+				this._showError(sResponse);
 			}
 		},
 
@@ -135,8 +143,63 @@ sap.ui.define([
 			let oFileUploader = this.byId("fileUploader");
 
 			oFileUploader.clear();
-			oFileUploader.setBusy(false);
+			this.getView().getModel().setProperty("/Busy", false);
 			oFileUploader.setFileType(["jpg", "png", "bmp"]);
-		}
+		},
+
+		classifyImage: function(factor0, factor1, factor2, amount0, amount1, amount2) {
+            let aPoints = [];
+            let sPathToSaveImageIn =  "./uploads/";
+
+            aPoints.push({
+                factor: Number(factor0),
+                amount: Number(amount0)
+            });
+
+            aPoints.push({
+                factor: Number(factor1),
+                amount: Number(amount1)
+            });
+            aPoints.push({
+                factor: Number(factor2),
+                amount: Number(amount2)
+            });
+
+            //Sortiere
+            aPoints.sort(function(a,b){
+                if (a.factor < b.factor) {
+                    return -1;
+                }
+
+                if (a.factor > b.factor) {
+                    return 1;
+                }
+                if (a.factor === b.factor) {
+                    if(a.amount < b.amount) {
+                        return -1;
+                    } else if (a.amount > b.amount ) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                }
+            });
+
+            //Setze den ersten Pfad zusammen
+            for(let i = 0; i < 3; i++) {
+                let oObject = aPoints[i];
+                if(oObject.amount !== 0) {
+
+                    let sAmount = oObject.amount < 10 ? "0" + oObject.amount : "" + oObject.amount;
+                    sPathToSaveImageIn += oObject.factor + sAmount;
+                }
+            }
+
+			if(aPoints[2].amount === 0) {
+				sPathToSaveImageIn += "0"
+			}
+			
+            return sPathToSaveImageIn;
+        }
 	});
 });
--- webapp/upload.php
+++ webapp/upload.php
@@ -1,44 +1,89 @@
 <?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
+
+//header('Content-Type: text/plain; charset=utf-8');
+
+try {
+    
+    // Undefined | Multiple Files | $_FILES Corruption Attack
+    // If this request falls under any of them, treat it invalid.
+    error_log( print_r($_FILES, TRUE) );
+    if (
+        !isset($_FILES['upfile']['error']) ||
+        is_array($_FILES['upfile']['error'])
+    ) {
+        throw new RuntimeException('Ungültige Parameter.');
     }
-}
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
+
+    // Check $_FILES['upfile']['error'] value.
+    switch ($_FILES['upfile']['error']) {
+        case UPLOAD_ERR_OK:
+            break;
+        case UPLOAD_ERR_NO_FILE:
+            throw new RuntimeException('Keine Datei mitgegeben.');
+        case UPLOAD_ERR_INI_SIZE:
+        case UPLOAD_ERR_FORM_SIZE:
+            throw new RuntimeException('Datei ist zu groß.');
+        default:
+            throw new RuntimeException('Unbekannter Fehler.');
     }
+
+    // You should also check filesize here. 
+    if ($_FILES['upfile']['size'] > 1000000) {
+        throw new RuntimeException('Datei ist zu groß.');
+    }
+
+    // DO NOT TRUST $_FILES['upfile']['mime'] VALUE !!
+    // Check MIME Type by yourself.
+    $finfo = new finfo(FILEINFO_MIME_TYPE);
+    if (false === $ext = array_search(
+        $finfo->file($_FILES['upfile']['tmp_name']),
+        array(
+            'jpg' => 'image/jpeg',
+            'png' => 'image/png',
+            'gif' => 'image/gif',
+        ),
+        true
+    )) {
+        throw new RuntimeException('Dateiformat ist nicht erlaubt');
+    }
+
+    error_log( print_r($_POST, TRUE) );
+    $folder = $_POST["Path"];
+
+    if ($folder === "" ) {
+        throw new RuntimeException('Es wurde keine Klassifizierung mitgegeben.');
+    }
+
+    $timestamp = time();
+
+    if (!file_exists($folder)) {
+        mkdir($folder, 0777, true);
+    }
+
+    // You should name it uniquely.
+    // DO NOT USE $_FILES['upfile']['name'] WITHOUT ANY VALIDATION !!
+    // On this example, obtain safe unique name from its binary data.
+    $newPath = sprintf('%s/%s_%s.%s',
+    $folder,
+    $timestamp,
+    sha1_file($_FILES['upfile']['tmp_name']),
+    $ext
+);
+error_log( print_r($newPath, TRUE) );
+    if (!move_uploaded_file(
+        $_FILES['upfile']['tmp_name'],
+        $newPath
+    )) {
+        throw new RuntimeException('Konnte die hochgeladene Datei nicht verschieben.');
+    }
+
+   // echo 'Datei erfolgreich hochgeladen.';
+    http_response_code(200);
+
+} catch (RuntimeException $e) {
+
+    echo $e->getMessage();
+    http_response_code(501);
 }
+
 ?>
